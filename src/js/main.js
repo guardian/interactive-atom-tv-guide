838a25b5c5311ac4e829d365d0965d9d30df31e6
blongden73 <hello@benlongdendesign.co.uk>
2018-03-06 17:05:26 +0000

    added styling for tv guide

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ el.src = '<%= path %>/app.js';
 document.body.appendChild(el);
 
 
-xr.get('https://interactive.guim.co.uk/docsdata-test/1oEb2VKND1R5H3UeDsi0Bf7265VMt4DieVcojviKTb-I.json').then((resp) => {
+xr.get('https://interactive.guim.co.uk/docsdata-test/1n6h_NXWt5MbHFvhkRhLqLw6ypKW0JQsz9Abf4TkYCiY.json').then((resp) => {
 		var sheets = resp.data.sheets;
 		var html = Mustache.render(blocksTemplate, sheets);
 
